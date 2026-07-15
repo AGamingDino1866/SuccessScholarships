e977d8003c822a6f22fbadfa766fe6f5a9f0fdcd
AGamingDino1866 <gamedino002@gmail.com>
2026-07-15 16:51:50 +0500

    Disable middleware geoblock pending approval

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,8 +1,12 @@
+const GEO_BLOCK_ENABLED = false;
 const COUNTRY_ALLOWLIST = new Set(["PK"]);
 const PUBLIC_PATHS = new Set(["/deny.html", "/favicon.svg"]);
 
 export async function onRequest(context) {
   const { request, next } = context;
+  // Flip GEO_BLOCK_ENABLED to true if Pakistan-only access is approved again.
+  if (!GEO_BLOCK_ENABLED) return next();
+
   const url = new URL(request.url);
   const pathname = url.pathname;
 
